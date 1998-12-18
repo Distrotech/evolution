205068b0585dac8da081dc0599bea05dfc705263
Elliot Lee <sopwith@src.gnome.org>
1998-12-18 04:29:03 +0000

    start implementing commands.
    start implementing commands.
    
    svn path=/branches/CALSERV_BRANCH/; revision=545

--- calendar/server/calserv.c
+++ calendar/server/calserv.c
@@ -1,12 +1,20 @@
 #include "calserv.h"
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
 
-static GSourceFuncs serv_fd_funcs = {NULL, NULL, &cs_connection_accept,
-				     &cs_connection_destroy};
+void cs_connection_accept(gpointer data, GIOCondition cond,
+			  CSServer *server);
+static void cs_connection_process(gpointer data, GIOCondition cond,
+				  CSConnection *cnx);
+static void cs_connection_greet(CSConnection *cnx);
+static void cs_connection_process_line(CSConnection *cnx, char *l);
+static void cs_connection_process_literal(CSConnection *cnx, char *l);
+
 CSServer *
 cs_server_new(void)
 {
@@ -33,7 +41,8 @@ cs_server_new(void)
   return rv;
 
  errout:
-  g_io_channel_unref(rv->gioc);
+  if(rv->gioc)
+    g_io_channel_unref(rv->gioc);
   close(rv->servfd);
   g_free(rv);
   return NULL;
@@ -50,7 +59,6 @@ cs_server_run(CSServer *server)
 void
 cs_server_destroy(CSServer *server)
 {
-  GList *ltmp, *nltmp;
   g_return_if_fail(server);
   
   g_main_quit(server->mainloop);
@@ -59,10 +67,7 @@ cs_server_destroy(CSServer *server)
   close(server->servfd);
   g_io_channel_unref(server->gioc);
 
-  for(ltmp = server->connections; ltmp; ltmp = nltmp) {
-    nltmp = ltmp->next;
-    cs_connection_destroy(ltmp->data);
-  }
+  g_list_foreach(server->connections, (GFunc)cs_connection_destroy, NULL);
 
   g_free(server);
 }
@@ -71,16 +76,85 @@ static void
 cs_connection_process(gpointer data, GIOCondition cond,
 		      CSConnection *cnx)
 {
-  if(cond & G_IO_HUP) {
+  char readbuf[257], *ctmp;
+  int rsize;
+  char *theline;
+
+  if(cond & (G_IO_HUP|G_IO_NVAL|G_IO_ERR)) {
     cs_connection_destroy(cnx);
     return;
   }
+
+  g_return_if_fail(cond & G_IO_IN);
+
+  /* read the data */
+  rsize = read(cnx->fd, readbuf, sizeof(readbuf) - 1);
+  readbuf[rsize] = '\0';
+  g_string_append(cnx->rdbuf, readbuf);
+
+  if(cnx->reading_literal) {
+    cnx->reading_literal -= rsize;
+
+    if(cnx->reading_literal <= 0) {
+      theline = g_strndup(cnx->rdbuf->str,
+			  cnx->rdbuf->len + cnx->reading_literal);
+      g_string_erase(cnx->rdbuf,
+		     0,
+		     cnx->rdbuf->len + cnx->reading_literal);
+      cnx->reading_literal = 0;
+      cs_connection_process_literal(cnx, theline);
+      g_free(theline);
+    }
+
+  } else {
+    ctmp = strchr(cnx->rdbuf->str, '\n');
+    if(!ctmp)
+      return; /* done reading the newly available data */
+    
+    /* OK, we have a complete line, save the rest */
+    theline = g_strndup(cnx->rdbuf->str, MAX(ctmp - cnx->rdbuf->str - 1, 0));
+    g_string_erase(cnx->rdbuf,
+		   0,
+		   MAX(ctmp - cnx->rdbuf->str + 1, cnx->rdbuf->len));
+    cs_connection_process_line(cnx, theline);
+    g_free(theline);
+  }
+}
+
+static void
+cs_connection_process_line(CSConnection *cnx, char *l)
+{
+  char *ctmp;
+  char *cmd_id, *cmd_name;
+
+  ctmp = l;
+
+  cmd_id = ctmp;
+  ctmp = strchr(ctmp, ' ');
+  g_return_if_fail(ctmp);
+  *ctmp = '\0';
+  ctmp++;
+
+  cmd_name = ctmp;
+  ctmp = strchr(ctmp, ' ');
+  if(ctmp) {
+    *ctmp = '\0';
+    ctmp++;
+  }
+
+  cs_connection_process_command(cnx, cmd_id, cmd_name, ctmp);
+}
+
+static void
+cs_connection_process_literal(CSConnection *cnx, char *l)
+{
+  g_error("NYI");
 }
 
 static void
 cs_connection_greet(CSConnection *cnx)
 {
-#define CS_greeting "* OK ICAP It is actually quite an aweful day today.\n"
+#define CS_greeting "* OK "CS_capabilities" It is actually quite an aweful day today.\n"
 
   write(cnx->fd, CS_greeting, sizeof(CS_greeting)-1);
 }
@@ -98,12 +172,21 @@ void cs_connection_accept(gpointer data, GIOCondition cond,
   if(fd < 0) return;
 
   cnx = g_new0(CSConnection, 1);
+  cnx->rdbuf = g_string_new(NULL);
   cnx->serv = server;
   cnx->fd = fd;
+  fcntl(cnx->fd, F_GETFL, &itmp);
+  itmp |= O_NONBLOCK;
+  fcntl(cnx->fd, F_SETFL, &itmp);  
+
+  cnx->fh = fdopen(fd, "a+");
+  setvbuf(cnx->fh, cnx->wrbuf, _IOLBF, sizeof(cnx->wrbuf));
+  g_assert(cnx->fh);
+
   cnx->gioc = g_io_channel_unix_new(fd);
   g_io_channel_ref(cnx->gioc);
   g_io_add_watch(cnx->gioc, G_IO_IN|G_IO_HUP|G_IO_NVAL,
-		 &cs_connection_process, cnx);
+		 (GIOFunc)&cs_connection_process, cnx);
 
   server->connections = g_list_prepend(server->connections, cnx);
   cs_connection_greet(cnx);
@@ -116,7 +199,9 @@ cs_connection_destroy(CSConnection *cnx)
 
   server = cnx->serv;
   server->connections = g_list_remove(server->connections, cnx);
-  close(cnx->fd);
+  fclose(cnx->fh); /* also closes fd */
   g_io_channel_unref(cnx->gioc);
+  g_string_free(cnx->rdbuf, TRUE);
+  g_free(cnx->authid);
   g_free(cnx);
 }     
--- calendar/server/calserv.h
+++ calendar/server/calserv.h
@@ -2,13 +2,16 @@
 #define CALSERV_H 1
 
 #include <glib.h>
+#include <stdio.h>
 
+#define CS_capabilities "ICAP AUTH=PLAINTEXT"
 typedef struct {
   int servfd;
   GList *connections;
   GMainLoop *mainloop;
   GIOChannel *gioc;
 } CSServer;
+/* in calserv.c */
 CSServer *cs_server_new(void);
 void cs_server_run(CSServer *server);
 void cs_server_destroy(CSServer *server);
@@ -17,13 +20,29 @@ typedef struct {
   CSServer *serv;
   GIOChannel *gioc;
   int fd;
+  FILE *fh;
+  GString *rdbuf;
+  char *authid; /* username, if authenticated */
+
+  GHashTable *curcmds; /* pending commands */
+  gint reading_literal; /* 0 if not currently reading */
+  char wrbuf[1024];
 } CSConnection;
-void cs_connection_accept(gpointer data, GIOCondition cond,
-			  CSServer *server);
+
+/* in commands.c */
+void cs_connection_process_command(CSConnection *cnx, char *cmd_id,
+				   char *cmd_name, char *rest_of_line);
+/* in calserv.c */
 void cs_connection_destroy(CSConnection *cnx);
 
 typedef char * CSIdentifier;
 CSIdentifier cs_identifier_new();
 #define cs_identifier_destroy(x) g_destroy(x)
+
+/* in auth.c */
+/* returns 0 if authentication succeeded, otherwise if not */
+gint cs_user_authenticate(CSConnection *cnx,
+			  const char *username,
+			  const char *password);
 
 #endif /* CALSERV_H */
