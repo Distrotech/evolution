bcbd6f6dd837dc99d401d339aa3aae1c9f951216
Nat Friedman <nat@src.gnome.org>
2001-10-28 01:39:51 +0000

    *** empty log message ***
    svn path=/branches/evolution-im/; revision=14258

--- im/messenger/e-messenger-listener.h
+++ im/messenger/e-messenger-listener.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * A client-side GtkObject which exposes the Evolution:IM:Listener
+ * A client-side GtkObject which exposes the Evolution:Messenger:Listener
  * interface.
  *
  * Author:
