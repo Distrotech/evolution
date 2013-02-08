04f2f0b35daf420215a18d8036d4de4eb22895dc
Matthew Barnes <mbarnes@redhat.com>
2013-02-08 11:24:39 -0500

    Remove e_mail_signature_editor_get_editor_widget().
    No longer used.

--- e-util/e-mail-signature-editor.h
+++ e-util/e-mail-signature-editor.h
@@ -25,7 +25,6 @@
 
 #include <libedataserver/libedataserver.h>
 
-#include <e-util/e-editor-widget.h>
 #include <e-util/e-editor-window.h>
 #include <e-util/e-focus-tracker.h>
 
@@ -84,8 +83,6 @@ gboolean	e_mail_signature_editor_commit_finish
 						(EMailSignatureEditor *editor,
 						 GAsyncResult *result,
 						 GError **error);
-EEditorWidget *	e_mail_signature_editor_get_editor_widget
-						(EMailSignatureEditor *editor);
 
 G_END_DECLS
 
